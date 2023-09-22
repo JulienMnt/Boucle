dc51d6fdbd14f4867c7d8a1dd988768cbd0109dd
JulienMnt <116883908+JulienMnt@users.noreply.github.com>
2023-09-22 16:00:44 +0200

    Add files via upload

---
+++ ex2.js
@@ -0,0 +1,13 @@
+/**            BOUCLE
+ *              FOR
+ * Declarer une variable a qui est egale à un entier positif
+ * 
+ * Faire un console log de 'OKLM' autant de fois que le valeur de a
+ * 
+ * 
+ */
+"use strict";
+let a = 5;
+for(let i=0 ; i < a ; i++){
+    console.log("OKLM");
+}
